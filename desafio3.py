7a798cddc7a612ab6c2a732a1c1dcfc8c3bc3d1b
Vitor MeirelesP <pintorgamer12@gmail.com>
2023-06-08 09:49:29 -0300

    fiz alterações no desafio 3 e 4

--- desafio3.py
+++ desafio3.py
@@ -8,5 +8,6 @@ A soma é 9
 '''
 numero1 = int(input('Primeiro número :'))
 numero2 = int(input('Segundo número :'))
-print('A some é', numero1 + numero2  )
-
+s = numero1 + numero2
+# print('A some é', numero1 + numero2  )
+print('A soma entre {} + {} = {}'.format(numero1,numero2,s))
--- desafio4.py
+++ desafio4.py
@@ -2,6 +2,9 @@
 Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo
 primitivo e todas as informações possíveis sobre ela 
 '''
-n = (input('Digitel algo:'))
-print(n.isnumeric())
+n = input('Digitel algo:')
 print(type(n))
+print('O valor é numerico ?{}'.format(n.isnumeric()))
+print('O valor é alfabetico e numnerico ?' ,n.isalnum())
+print('O valor é numerico ?' ,n.isalpha())
+print('O valor é decimal ?' ,n.isdecimal())
